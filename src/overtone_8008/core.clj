40b85152069aaa1658e3ef4b2df9eccb24dc6ce1
jj <jj.weber@gmail.com>
2016-04-23 12:26:33 -0700

    Synths

--- src/overtone_8008/core.clj
+++ src/overtone_8008/core.clj
@@ -6,3 +6,16 @@
   (* (sin-osc freq (* Math/PI 0.5))
      (env-gen (perc 0 decay) 1 1 0 1 FREE)
      amp))
+
+;; low buzz
+(definst lobuzz [] (lpf (saw 100) 500))
+
+;; example interesting sound
+(defsynth bizzle [out-bus 10 amp 0.5]
+  (out out-bus
+       (* amp
+          (+ (* (decay2 (* (impulse 10 0)
+                           (+ (* (lf-saw:kr 0.3 0) -0.3) 0.3))
+                        0.001)
+                0.3)
+             (apply + (pulse [80 81]))))))
